5c2a1ea1eccaab141939e96fa5868552922ab5ef
Sergey Titov <ya.serge-vladimirov@yandex.ru>
2022-03-23 22:09:36 +0300

    Add test action

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/setov/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/setov/frontend-project-lvl2/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/413def6ffa2e14204f32/maintainability)](https://codeclimate.com/github/setov/frontend-project-lvl2/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/413def6ffa2e14204f32/test_coverage)](https://codeclimate.com/github/setov/frontend-project-lvl2/test_coverage)
+
+
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,11 +3,13 @@ import { readFile, buildPath } from '../src/genDiff.js';
 
 const getFixturePath = (filename) => buildPath(['__fixtures__', filename]);
 
-const expectedData = { plain: [] };
+const expectedData = { plain: [], complex: [] };
 
 beforeAll(() => {
   const plainData = readFile([getFixturePath('expected_flat.txt')]);
+  const complexData = readFile([getFixturePath('expected_complex.txt')]);
   expectedData.plain = plainData.trim();
+  expectedData.complex = complexData.trim();
 });
 
 test('plain object', () => {
@@ -16,5 +18,14 @@ test('plain object', () => {
 
   const actual = gendiff(filePath1, filePath2);
   const expected = expectedData.plain;
+  expect(actual).toEqual(expected);
+});
+
+test('complex object', () => {
+  const filePath1 = getFixturePath('complexFile1.json');
+  const filePath2 = getFixturePath('complexFile2.json');
+
+  const actual = gendiff(filePath1, filePath2);
+  const expected = expectedData.complex;
   expect(actual).toEqual(expected);
 });
