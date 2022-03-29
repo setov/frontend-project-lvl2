cce5d0bb40de1a3a923083508c16d1b6dea94fd9
Sergey Titov <ya.serge-vladimirov@yandex.ru>
2022-03-29 09:26:37 +0300

    eight step. Add json format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,14 +3,16 @@ import { readFile, buildPath } from '../src/fileUtils.js';
 
 const getFixturePath = (filename) => buildPath(['__fixtures__', filename]);
 
-const expectedData = { stylish: [], plain: [] };
-const formats = ['stylish', 'plain'];
+const expectedData = { stylish: [], plain: [], json: [] };
+const formats = ['stylish', 'plain', 'json'];
 
 beforeAll(() => {
   const complexDataStylish = readFile([getFixturePath('expected_complex.txt')]);
   const complexDataPlain = readFile([getFixturePath('expected_plain.txt')]);
+  const complexDataJson = readFile([getFixturePath('expected_json.json')]);
   expectedData.stylish = complexDataStylish.trim();
   expectedData.plain = complexDataPlain.trim();
+  expectedData.json = complexDataJson.trim();
 });
 
 test('complex object json format', () => {
@@ -40,5 +42,15 @@ test('complex object plain format', () => {
   const [, plain] = formats;
   const actual = gendiff(filePath1, filePath2, plain);
   const expected = expectedData.plain;
+  expect(actual).toEqual(expected);
+});
+
+test('complex object json format', () => {
+  const filePath1 = getFixturePath('complexFile1.json');
+  const filePath2 = getFixturePath('complexFile2.json');
+
+  const [,, json] = formats;
+  const actual = gendiff(filePath1, filePath2, json);
+  const expected = expectedData.json;
   expect(actual).toEqual(expected);
 });
