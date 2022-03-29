b415d0075a6dbf7195aa4d8265498e2d42ec870f
Sergey Titov <ya.serge-vladimirov@yandex.ru>
2022-03-29 22:14:21 +0300

    refactoring stylish.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,30 +10,30 @@ const stringify = (value) => {
   return value;
 };
 
-const plainAst = (ast) => {
-  const stringByNodeType = (node, ancestor, fun) => {
-    const {
-      name, type, valueBefore, valueAfteer, children,
-    } = node;
-    const newAncestor = ancestor === '' ? `${name}` : `${ancestor}.${name}`;
-    const beforeValue = stringify(valueBefore);
-    const afterValue = stringify(valueAfteer);
-    switch (type) {
-      case 'unchanged':
-        return '';
-      case 'updated': {
-        return `Property '${newAncestor}' was updated. From ${beforeValue} to ${afterValue}`;
-      }
-      case 'removed':
-        return `Property '${newAncestor}' was removed`;
-      case 'added':
-        return `Property '${newAncestor}' was added with value: ${afterValue}`;
-      case 'complex':
-        return fun(children, newAncestor);
-      default: throw new Error(`unexpected type ${type}`);
+const stringByNodeType = (node, ancestor, fun) => {
+  const {
+    name, type, valueBefore, valueAfteer, children,
+  } = node;
+  const newAncestor = ancestor === '' ? `${name}` : `${ancestor}.${name}`;
+  const beforeValue = stringify(valueBefore);
+  const afterValue = stringify(valueAfteer);
+  switch (type) {
+    case 'unchanged':
+      return '';
+    case 'updated': {
+      return `Property '${newAncestor}' was updated. From ${beforeValue} to ${afterValue}`;
     }
-  };
+    case 'removed':
+      return `Property '${newAncestor}' was removed`;
+    case 'added':
+      return `Property '${newAncestor}' was added with value: ${afterValue}`;
+    case 'complex':
+      return fun(children, newAncestor);
+    default: throw new Error(`unexpected type ${type}`);
+  }
+};
 
+const plainAst = (ast) => {
   const iter = (nodes, ancestor) => {
     const lines = nodes
       .map((node) => stringByNodeType(node, ancestor, iter));
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,49 +1,52 @@
 import _ from 'lodash';
 
-const stringifyAst = (ast, replacer = ' ', spacesCount = 2) => {
-  const stringify = (value, depth) => {
-    const iter = (currentValue, depthIn) => {
-      if (!(typeof currentValue === 'object') || _.isNull(currentValue)) {
-        return currentValue;
-      }
-      const indentSize = (depthIn + spacesCount) * spacesCount;
-      const currentIndent = replacer.repeat(indentSize + spacesCount);
-      const bracketIndent = replacer.repeat(indentSize - spacesCount);
+const REPLACER = ' ';
+const SPACES_COUNT = 2;
 
-      const lines = Object.entries(currentValue).map(([key, val]) => `${currentIndent}${key}: ${iter(val, depthIn + 2)}`);
-      return ['{', ...lines, `${bracketIndent}}`].join('\n');
-    };
-    return iter(value, depth);
-  };
-
-  const stringByNodeType = (node, depth, fun) => {
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const complexIndent = replacer.repeat(indentSize + spacesCount);
-    const {
-      name, type, valueBefore, valueAfteer, children,
-    } = node;
-    switch (type) {
-      case 'unchanged':
-        return `${currentIndent}  ${name}: ${stringify(valueBefore, depth)}`;
-      case 'updated': {
-        const line1 = `${currentIndent}- ${name}: ${stringify(valueBefore, depth)}`;
-        const line2 = `${currentIndent}+ ${name}: ${stringify(valueAfteer, depth)}`;
-        return line1.concat('\n', line2);
-      }
-      case 'removed':
-        return `${currentIndent}- ${name}: ${stringify(valueBefore, depth)}`;
-      case 'added':
-        return `${currentIndent}+ ${name}: ${stringify(valueAfteer, depth)}`;
-      case 'complex':
-        return `${complexIndent}${name}: ${fun(children, depth + spacesCount)}`;
-      default: throw new Error(`unexpected type ${type}`);
+const stringify = (value, depth) => {
+  const iter = (currentValue, depthIn) => {
+    if (!(typeof currentValue === 'object') || _.isNull(currentValue)) {
+      return currentValue;
     }
-  };
+    const indentSize = (depthIn + SPACES_COUNT) * SPACES_COUNT;
+    const currentIndent = REPLACER.repeat(indentSize + SPACES_COUNT);
+    const bracketIndent = REPLACER.repeat(indentSize - SPACES_COUNT);
 
+    const lines = Object.entries(currentValue).map(([key, val]) => `${currentIndent}${key}: ${iter(val, depthIn + 2)}`);
+    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+  };
+  return iter(value, depth);
+};
+
+const stringByNodeType = (node, depth, fun) => {
+  const indentSize = depth * SPACES_COUNT;
+  const currentIndent = REPLACER.repeat(indentSize);
+  const complexIndent = REPLACER.repeat(indentSize + SPACES_COUNT);
+  const {
+    name, type, valueBefore, valueAfteer, children,
+  } = node;
+  switch (type) {
+    case 'unchanged':
+      return `${currentIndent}  ${name}: ${stringify(valueBefore, depth)}`;
+    case 'updated': {
+      const line1 = `${currentIndent}- ${name}: ${stringify(valueBefore, depth)}`;
+      const line2 = `${currentIndent}+ ${name}: ${stringify(valueAfteer, depth)}`;
+      return line1.concat('\n', line2);
+    }
+    case 'removed':
+      return `${currentIndent}- ${name}: ${stringify(valueBefore, depth)}`;
+    case 'added':
+      return `${currentIndent}+ ${name}: ${stringify(valueAfteer, depth)}`;
+    case 'complex':
+      return `${complexIndent}${name}: ${fun(children, depth + SPACES_COUNT)}`;
+    default: throw new Error(`unexpected type ${type}`);
+  }
+};
+
+const stringifyAst = (ast) => {
   const iter = (nodes, depth) => {
-    const indentSize = depth * spacesCount;
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const indentSize = depth * SPACES_COUNT;
+    const bracketIndent = REPLACER.repeat(indentSize - SPACES_COUNT);
     const lines = nodes
       .map((node) => stringByNodeType(node, depth, iter));
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
